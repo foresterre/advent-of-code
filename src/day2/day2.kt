a9087cf34778ae639b6b6053292ea5f8b0367a10
foresterre <garm@ilumeo.com>
2017-12-12 21:02:40 +0100

    Add day 2: Corruption Checksum, part 2.

--- src/day2/day2.kt
+++ src/day2/day2.kt
@@ -10,16 +10,39 @@ import Lib.readLines
 fun main(args: Array<String>) {
 
     val splitByRows = readLines("resources/day2/input.in")
-    val spreadsheetAsStrings = splitByRows.map { i -> i.split("\t") }
-    val spreadsheetAsInts = spreadsheetAsStrings.map { line -> line.map { value -> Integer.valueOf(value) } }
+    val spreadsheetAsStrings = splitByRows.map { it.split("\t") }
+    val spreadsheetAsInts = spreadsheetAsStrings.map { it.map { Integer.valueOf(it) } }
 
     // part 1
     println(checksum(spreadsheetAsInts))
+
+    // part 2
+    println(evenlyDivisibleSum(spreadsheetAsInts))
 }
 
 fun checksum(spreadsheet: List<List<Int>>): Int {
     fun min(collection: Collection<Int>): Int = collection.min() as Int
     fun max(collection: Collection<Int>): Int = collection.max() as Int
 
-    return spreadsheet.map { i -> max(i) - min(i) }.sum()
+    return spreadsheet.map { max(it) - min(it) }.sum()
 }
+
+fun evenlyDivisibleSum(spreadsheet: List<List<Int>>): Int {
+    fun isEvenlyDivisible(pair: Pair<Int, Int>): Boolean = pair.first % pair.second == 0
+
+    val uniques = spreadsheet.map { it.toSet() }
+    val divisibleCombinations = uniques.map {
+        val combo = combinations(it).first { pair ->
+            pair.first != pair.second && isEvenlyDivisible(pair)
+        }
+        combo.first / combo.second
+    }
+
+    return divisibleCombinations.sum()
+}
+
+fun combinations(set: Set<Int>): List<Pair<Int, Int>> {
+    return set.flatMap { i -> set.map { j -> Pair(i, j) } }
+}
+
+
